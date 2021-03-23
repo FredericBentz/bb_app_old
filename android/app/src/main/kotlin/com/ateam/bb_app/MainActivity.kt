ea57d2192551f45de86bd8fa12b369e65884b013
FredericBentz <frederic.bentz@gmail.com>
2021-03-23 10:32:24 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/ateam/bb_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ateam.bb_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/users.dart
@@ -0,0 +1,6 @@
+const mockUsers = {
+  'frederic@gmail.com': 'serge',
+  'serge@gmail.com': 'serge',
+  'mamadou@gmail.com': 'mamadou',
+  'olivier@gmail.com': 'olivier',
+};
